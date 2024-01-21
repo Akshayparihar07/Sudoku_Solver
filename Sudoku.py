677ab4e6e01fc0d8e55fbe116fd731445f978801
Akshay Parihar <pariharakshay40@gmail.com>
2024-01-21 16:18:58 +0530

    fixed board selection

--- Sudoku.py
+++ Sudoku.py
@@ -1,5 +1,5 @@
+import time
 import random
-board = random_boards[random.randint(0, 11)]
 
 def print_board(board, size=9):
     for i in range(size):
@@ -11,19 +11,13 @@ def print_board(board, size=9):
             print(board[i][j], end=' ')
         print()
 
-
 def solve_sudoku(board):
+    empty_cell = find_empty(board)
 
-    # BASE CASE
-    find = find_empty(board)
-
-    # IF NOT 0s (EMPTY CELLS FOUND) THEN RETURN TRUE AS WE HAVE REACHED THE END OF OUR BOARD 
-    if not find :
+    if not empty_cell:
         return True
-    
-    # OTHERWISE TAKE THE ROW AND COL RETURNED BY find_empty() FUNCITON
-    else:
-        (row, col) = find
+
+    row, col = empty_cell
 
     for num in range(1, 10):
         position = (row, col)
@@ -35,44 +29,48 @@ def solve_sudoku(board):
                 board[row][col] = 0
     return False
 
-    
-
 def is_valid(board, num, position):
-    r = position[0]
-    c = position[1]
+    r, c = position
 
     for i in range(9):
-        # Checking all the columns of a row  
         if board[r][i] == num and c != i:
             return False
 
-        # checking all the rows of a column 
         if board[i][c] == num and r != i:
             return False
 
-        # Check in the 3x3 grid
-        box_x = position[1]
-        box_y = position[0]
+    box_x, box_y = c // 3, r // 3
 
-        for i in range(box_y*3, box_y*3 + 3):
-            for j in range(box_x*3, box_x*3 + 3):
-                if i < 9 and j < 9 and board[i][j] == num and (i,j) != position:
-                    return False
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if i < 9 and j < 9 and board[i][j] == num and (i, j) != position:
+                return False
     return True
-
 
 def find_empty(board):
     for x in range(9):
         for y in range(9):
-        # IF FOUND ANY EMPTY SPACE AT ANY PARTICULAR POSITION THEN RETURN THE POSITION 
             if board[x][y] == 0:
-                return (x, y)  # this will return the coordinates in the form if a tuple 
-
-    # OTHERWISE REUTRN NOTHING
+                return x, y
     return None
 
-if __name__=="__main__":
+if __name__ == "__main__":
+    board =     [[0, 0, 4, 0, 0, 0, 0, 0, 0],
+                [2, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 6, 0, 0, 0, 0, 8],
+                [0, 0, 0, 0, 0, 0, 5, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 9, 8, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 7, 0],
+                [0, 0, 0, 0, 3, 0, 0, 0, 2],
+                [0, 0, 0, 0, 7, 9, 0, 0, 0]]
+    
+    print('Original Sudoku:')
     print_board(board)
+
     solve_sudoku(board)
-    print()
+
+    print('\nSolving Sudoku⌛')
+    time.sleep(2)
+    print('\nHere is Your Solved Sudoku🥳')
     print_board(board)
